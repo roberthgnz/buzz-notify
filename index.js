71c1916ef8a4aeb2bea819c8c98512e32fc27cf1
Roberth <reliutg@gmail.com>
2020-08-10 18:53:22 +0200

    Icons added

--- index.js
+++ index.js
@@ -49,6 +49,17 @@ const cssText = `#notify {
   border: 1px solid transparent;
   border-radius: 0.25rem;
   transition: all 300ms ease 0s;
+  user-select: none;
+  cursor: pointer;
+}
+.notify__title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row-reverse;
+}
+.notify__title svg {
+  margin-right: 7.5px;
 }
 .notify--success {
   color: #155724;
@@ -109,9 +120,38 @@ function Notify(
   setTimeout(() => notifyContent.classList.remove("animate"), 300);
 
   notifyContent.innerHTML = `
-        <div style="font-weight: 700;color: #0b2e13;">${title}</div>
+        <div class="notify__title" style="font-weight: 700;color: #0b2e13;">${title}</div>
         ${html !== null ? html : ""}
     `;
+
+  const notifyTitle = notifyContent.querySelector(".notify__title");
+
+  switch (type) {
+    case "success":
+      notifyTitle.innerHTML += `
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#155724" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z"/>
+      <circle cx="12" cy="12" r="9" />
+      <path d="M9 12l2 2l4 -4" />
+    </svg>`;
+      break;
+    case "warning":
+      notifyTitle.innerHTML += `
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#856404" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <circle cx="12" cy="12" r="9" />
+        <line x1="12" y1="8" x2="12" y2="12" />
+        <line x1="12" y1="16" x2="12.01" y2="16" />
+      </svg>`;
+      break;
+    case "danger":
+      notifyTitle.innerHTML += `
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-triangle" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#721c24" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <path d="M12 9v2m0 4v.01" />
+        <path d="M5.07 19H19a2 2 0 0 0 1.75 -2.75L13.75 4a2 2 0 0 0 -3.5 0L3.25 16.25a2 2 0 0 0 1.75 2.75" />
+      </svg>`;
+  }
 
   if (position.split(" ")[0] === "top") {
     notifyWrapper.insertAdjacentElement("afterbegin", notifyContent);
@@ -127,4 +167,8 @@ function Notify(
       notifyContent.remove();
     }, duration);
   }
+
+  notifyContent.addEventListener("click", function () {
+    this.remove();
+  });
 }
