371b455ec180588cb1cb32700513df5309f3d442
eliutgon <reliutg@gmail.com>
2021-10-04 19:06:28 +0200

    fix: undefined notify

--- src/index.ts
+++ src/index.ts
@@ -58,8 +58,6 @@ const icons = {
   danger: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#721c24" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M12 9v2m0 4v.01" /><path d="M5.07 19H19a2 2 0 0 0 1.75 -2.75L13.75 4a2 2 0 0 0 -3.5 0L3.25 16.25a2 2 0 0 0 1.75 2.75" /></svg>`,
 };
 
-const notify = document.querySelector("#notify")!;
-
 const TRANSITION_DURATION = 400; // ms
 
 /**
@@ -79,6 +77,8 @@ function Notify(
   }: NotifyOptions,
   callback: () => void
 ) {
+  const notify = document.querySelector("#notify")!;
+  
   if (!notify.querySelector(`[data-notify='${position}']`)) {
     const notifyWrapper = document.createElement("div");
 
