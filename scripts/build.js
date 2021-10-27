89bf74c3b28da234dce08511d1d1cbad0e8ddd1f
Eliut González <reliutg@gmail.com>
2021-10-27 09:56:35 +0200

    fix: iife export

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
-const esbuild = require('esbuild');
-const package = require('../package.json');
-const fs = require('fs');
+const esbuild = require('esbuild')
+const package = require('../package.json')
+const fs = require('fs')
 
 const commonOptions = {
   entryPoints: ['src/index.ts'],
@@ -9,25 +9,27 @@ const commonOptions = {
     js: `// ${package.author}`,
     css: `/*${package.author}*/`,
   },
-};
+}
 
 // ECMAScript Module
 esbuild.buildSync({
   ...commonOptions,
   format: 'esm',
   outdir: 'dist/esm',
-});
+})
 
 // IIFE
 // Read dist/esm/index.js
-const esm = fs.readFileSync('dist/esm/index.js', 'utf8').replace(/}\)};export{([a-z]) as Notify};/g, '})};return $1;');
+const esm = fs
+  .readFileSync('dist/esm/index.js', 'utf8')
+  .replace(/}\)};export{([a-z]) as Notify,[a-z] as NotifyAsync};/g, '})};return $1;')
 
 // Write dist/index.js
-fs.writeFileSync('dist/index.js', `var Notify = (() => {${esm}})();`);
+fs.writeFileSync('dist/index.js', `var Notify = (() => {${esm}})();`)
 
 // CSS
 esbuild.buildSync({
   ...commonOptions,
   entryPoints: ['src/style.css'],
   outfile: 'dist/buzz-notify.css',
-});
+})
